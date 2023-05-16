20fdff27e2fb4e26d94a7740f4f844fba0b04713
chandrucrayond <chandru@crayond.co>
2023-05-16 21:42:15 +0530

    AT 9.40pm I had found the mistake, by placing script in head, but there is problem in selecting the element , im just replacing with alternate2.html script only so that revert it back to it easily

--- js/script.js
+++ js/script.js
@@ -1,110 +1,128 @@
-var jsonData = {
-  pixel_5: {
-    id: "pixel_5",
-    image: "pixel_5.jpeg",
-    name: "Pixel 5",
-    price: "$ 499",
-    feature: "Smart, Powerful, Helpful",
-    rating: "1.2k",
-    color: ["Black", "White", "Blue"],
-    display: [
-      "Full-screen 6.1-inch (156 mm)1 display, up to 60 Hz",
-      "20:9 aspect ratio",
-      "FHD+ (1080 x 2400) OLED at 429 ppi",
-      "Corning® Gorilla® Glass 3™ cover glass",
-      "Always-on display with At a Glance and Now Playing",
-    ],
-    dimension: [
-      "6. heightt x 2.8 width x 0.35 depth (in)",
-      "152.2 height x 71.8 width x 8.9 depth (mm)",
-      "6.3 oz",
-      "178 g",
-    ],
-    battery: [
-      "Over 24-hour battery life3",
-      "Up to 72-our battery life with Extreme Battery-Saver",
-      "Typical 4410 mAh",
-      "Fast Charging",
-    ],
-    memory: "6 GB LPDDR5 RAM",
-    storage: "128 GB storage UFS 3.1 storage",
-  },
-  pixel_6: {
-    id: "pixel_6",
-    image: "pixel_6.jpg",
-    name: "Pixel 6",
-    price: "$ 699",
-    feature: "Smart",
-    rating: "1.2k",
-    color: ["Black", "White", "Blue"],
-    display: [
-      "Full-screen 6.1-inch (156 mm)1 display, up to 60 Hz",
-      "20:9 aspect ratio",
-      "FHD+ (1080 x 2400) OLED at 429 ppi",
-      "Corning® Gorilla® Glass 3™ cover glass",
-      "Always-on display with At a Glance and Now Playing",
-    ],
-    dimension: [
-      "6. heightt x 2.8 width x 0.35 depth (in)",
-      "152.2 height x 71.8 width x 8.9 depth (mm)",
-      "6.3 oz",
-      "178 g",
-    ],
-    battery: [
-      "Over 24-hour battery life3",
-      "Up to 72-our battery life with Extreme Battery-Saver",
-      "Typical 4410 mAh",
-      "Fast Charging",
-    ],
-    memory: "6 GB LPDDR5 RAM",
-    storage: "128 GB storage UFS 3.1 storage",
-    processor: "Google Tensor Titan M2 security coprocessor",
-  },
+// var jsonData = {
+//   pixel_5: {
+//     id: "pixel_5",
+//     image: "pixel_5.jpeg",
+//     name: "Pixel 5",
+//     price: "$ 499",
+//     feature: "Smart, Powerful, Helpful",
+//     rating: "1.2k",
+//     color: ["Black", "White", "Blue"],
+//     display: [
+//       "Full-screen 6.1-inch (156 mm)1 display, up to 60 Hz",
+//       "20:9 aspect ratio",
+//       "FHD+ (1080 x 2400) OLED at 429 ppi",
+//       "Corning® Gorilla® Glass 3™ cover glass",
+//       "Always-on display with At a Glance and Now Playing",
+//     ],
+//     dimension: [
+//       "6. heightt x 2.8 width x 0.35 depth (in)",
+//       "152.2 height x 71.8 width x 8.9 depth (mm)",
+//       "6.3 oz",
+//       "178 g",
+//     ],
+//     battery: [
+//       "Over 24-hour battery life3",
+//       "Up to 72-our battery life with Extreme Battery-Saver",
+//       "Typical 4410 mAh",
+//       "Fast Charging",
+//     ],
+//     memory: "6 GB LPDDR5 RAM",
+//     storage: "128 GB storage UFS 3.1 storage",
+//   },
+//   pixel_6: {
+//     id: "pixel_6",
+//     image: "pixel_6.jpg",
+//     name: "Pixel 6",
+//     price: "$ 699",
+//     feature: "Smart",
+//     rating: "1.2k",
+//     color: ["Black", "White", "Blue"],
+//     display: [
+//       "Full-screen 6.1-inch (156 mm)1 display, up to 60 Hz",
+//       "20:9 aspect ratio",
+//       "FHD+ (1080 x 2400) OLED at 429 ppi",
+//       "Corning® Gorilla® Glass 3™ cover glass",
+//       "Always-on display with At a Glance and Now Playing",
+//     ],
+//     dimension: [
+//       "6. heightt x 2.8 width x 0.35 depth (in)",
+//       "152.2 height x 71.8 width x 8.9 depth (mm)",
+//       "6.3 oz",
+//       "178 g",
+//     ],
+//     battery: [
+//       "Over 24-hour battery life3",
+//       "Up to 72-our battery life with Extreme Battery-Saver",
+//       "Typical 4410 mAh",
+//       "Fast Charging",
+//     ],
+//     memory: "6 GB LPDDR5 RAM",
+//     storage: "128 GB storage UFS 3.1 storage",
+//     processor: "Google Tensor Titan M2 security coprocessor",
+//   },
 
-  pixel_6_pro: {
-    id: "pixel_6_pro",
-    image: "pixel_6pro.jpg",
-    name: "Pixel 6 Pro",
-    price: "$ 799",
-    feature: "Powerful, Helpful",
-    rating: "1.2k",
-    color: ["Black", "White", "Blue"],
-    display: [
-      "Full-screen 6.1-inch (156 mm)1 display, up to 60 Hz",
-      "20:9 aspect ratio",
-      "FHD+ (1080 x 2400) OLED at 429 ppi",
-      "Corning® Gorilla® Glass 3™ cover glass",
-      "Always-on display with At a Glance and Now Playing",
-    ],
-    dimension: [
-      "6. heightt x 2.8 width x 0.35 depth (in)",
-      "152.2 height x 71.8 width x 8.9 depth (mm)",
-      "6.3 oz",
-      "178 g",
-    ],
-    battery: [
-      "Over 24-hour battery life3",
-      "Up to 72-our battery life with Extreme Battery-Saver",
-      "Typical 4410 mAh",
-      "Fast Charging",
-    ],
-    memory: "6 GB LPDDR5 RAM",
-    storage: "128 GB storage UFS 3.1 storage",
-  },
-};
-var cardLists = document.getElementsByClassName("card");
-for (let i = 0; i < cardLists.length; i++) {
-  (function (index) {
-    console.log("hello world");
-    cardLists[index].addEventListener("mouseenter", function () {
-      hoverFunction(index);
-    });
-    cardLists[index].addEventListener("mouseleave", function () {
-      hoverRemoveFunction(index);
-    });
-  })(i);
+//   pixel_6_pro: {
+//     id: "pixel_6_pro",
+//     image: "pixel_6pro.jpg",
+//     name: "Pixel 6 Pro",
+//     price: "$ 799",
+//     feature: "Powerful, Helpful",
+//     rating: "1.2k",
+//     color: ["Black", "White", "Blue"],
+//     display: [
+//       "Full-screen 6.1-inch (156 mm)1 display, up to 60 Hz",
+//       "20:9 aspect ratio",
+//       "FHD+ (1080 x 2400) OLED at 429 ppi",
+//       "Corning® Gorilla® Glass 3™ cover glass",
+//       "Always-on display with At a Glance and Now Playing",
+//     ],
+//     dimension: [
+//       "6. heightt x 2.8 width x 0.35 depth (in)",
+//       "152.2 height x 71.8 width x 8.9 depth (mm)",
+//       "6.3 oz",
+//       "178 g",
+//     ],
+//     battery: [
+//       "Over 24-hour battery life3",
+//       "Up to 72-our battery life with Extreme Battery-Saver",
+//       "Typical 4410 mAh",
+//       "Fast Charging",
+//     ],
+//     memory: "6 GB LPDDR5 RAM",
+//     storage: "128 GB storage UFS 3.1 storage",
+//   },
+// };
+var cardLists = document.getElementsByClassName("full_carde");
+
+for (i = 0; i < cardLists.length; i++) {
+  console.log(i);
+  cardLists[i].addEventListener("mouseenter", function () {
+    hoverFunction(i);
+  });
+  cardLists[i].addEventListener("mouseleave", function () {
+    hoverRemoveFunction(i);
+  });
 }
+
+// var elements = document.getElementsByClassName("full_carde");
+// console.log(document.getElementsByClassName("full_carde").length);
+// for (var i = 0; i < elements.length; i++) {
+//   var element = elements[i];
+//   console.log("hello2");
+//   element.addEventListener("mouseenter", function () {
+//     // Hovering code
+//     console.log("Mouse entered:", this);
+//   });
+
+//   element.addEventListener("mouseleave", function () {
+//     // Not hovering code
+//     console.log("Mouse left:", this);
+//   });
+// }
+
 function hoverFunction(i) {
+  var cardLists = document.getElementsByClassName("full_carde");
+
   var featureSection = cardLists[i].querySelector(
     ".main--para__productFeature"
   );
@@ -121,6 +139,7 @@ function hoverFunction(i) {
   var productsGrid = cardLists[i].querySelector(
     ".main--section__productDetailsGrid"
   );
+
   featureSection.classList.add("hidden");
   viewProductButton.classList.add("hovered");
   featureSection.innerHTML = viewProductButton.outerHTML;
@@ -132,6 +151,7 @@ function hoverFunction(i) {
 }
 
 function hoverRemoveFunction(i) {
+  var cardLists = document.getElementsByClassName("full_carde");
   var featureSection = cardLists[i].querySelector(
     ".main--para__productFeature"
   );
