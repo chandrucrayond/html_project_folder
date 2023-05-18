7a753c760f118c9d38bd1805374e54d94badde1d
chandrucrayond <chandru@crayond.co>
2023-05-18 12:55:31 +0530

    modal going successfully

--- js/script.js
+++ js/script.js
@@ -260,3 +260,22 @@ for (var i = 0; i < cardLists.length; i++) {
     }
   })(i);
 }
+
+const exampleModal = document.getElementById('exampleModal')
+if (exampleModal) {
+  exampleModal.addEventListener('show.bs.modal', event => {
+    // Button that triggered the modal
+    const button = event.relatedTarget
+    // Extract info from data-bs-* attributes
+    const recipient = button.getAttribute('data-bs-whatever')
+    // If necessary, you could initiate an Ajax request here
+    // and then do the updating in a callback.
+
+    // Update the modal's content.
+    const modalTitle = exampleModal.querySelector('.modal-title')
+    const modalBodyInput = exampleModal.querySelector('.modal-body input')
+
+    modalTitle.textContent = `Buy Pixel ${recipient}`
+    modalBodyInput.value = recipient
+  })
+}
